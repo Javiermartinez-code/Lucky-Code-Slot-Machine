ad5f4c08d7e6396d1dd85075290da03cde7cf96c
Javi Martinez Vilaplana <javier@MacBook-Pro-de-Javi.local>
2026-01-03 20:39:52 +0100

    Slot view finished

--- src/me/javier/luckycode/Main.java
+++ src/me/javier/luckycode/Main.java
@@ -1,9 +1,27 @@
 package me.javier.luckycode;
 
-import me.javier.luckycode.model.*;
+import me.javier.luckycode.model.reel.Reel;
+import me.javier.luckycode.model.symbols.Symbol;
+import me.javier.luckycode.view.SlotView;
 
 public class Main {
     public static void main(String[] args) {
+        Reel testReel = new Reel();
+        SlotView view = new SlotView();
+        // 1. El rodillo decide el símbolo ganador de forma aleatoria
+        Symbol randomResult = testReel.getRandomSymbol();
 
+        System.out.println("Sorteando premio...");
+
+        try {
+            // 2. Ejecutamos la cascada pasándole ese resultado aleatorio
+            view.drawCascade(testReel, randomResult);
+
+            System.out.println("\n--- RESULTADO FINAL ---");
+            System.out.println("Símbolo obtenido: " + randomResult.getName());
+
+        } catch (InterruptedException e) {
+                    System.out.println("Error en la animación.");
+        }
     }
 }
---
+++ src/me/javier/luckycode/view/SlotView.java
@@ -0,0 +1,22 @@
+package me.javier.luckycode.view;
+
+import me.javier.luckycode.model.reel.Reel;
+import me.javier.luckycode.model.symbols.Symbol;
+
+public class SlotView {
+    public void drawCascade(Reel reel, Symbol finalSymbol) throws InterruptedException {
+        int spins = 15; // Simbolos que aparecen antes de frenarse.
+
+        for (int i = 0; i < spins; i++) {
+            System.out.println("\n".repeat(30));
+
+            Symbol showing = (i == spins - 1) ? finalSymbol : reel.getRandomSymbol();
+            System.out.println("      [ RODILLO ]");
+            System.out.println("-------------------------");
+            System.out.println(showing.getAsciiPresentation());
+            System.out.println("-------------------------");
+
+            Thread.sleep(100 + (i * 20));
+        }
+    }
+}
