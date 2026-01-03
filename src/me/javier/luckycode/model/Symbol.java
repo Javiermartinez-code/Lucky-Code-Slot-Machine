7dd23c49ac2ac2da737b4314cc552fe654a03a06
Javi Martinez Vilaplana <javier@MacBook-Pro-de-Javi.local>
2026-01-03 14:14:01 +0100

    Begin Symbol class

---
+++ src/me/javier/luckycode/model/Symbol.java
@@ -0,0 +1,25 @@
+package me.javier.luckycode.model;
+
+public class Symbol {
+    private String name;
+    private int scoreValue;
+    private String asciiPresentation;
+
+    public Symbol(String name, int scoreValue, String asciiPresentation) {
+        this.name = name;
+        this.scoreValue = scoreValue;
+        this.asciiPresentation = asciiPresentation;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getScoreValue() {
+        return this.scoreValue;
+    }
+
+    public String getAsciiPresentation() {
+        return this.asciiPresentation;
+    }
+}
